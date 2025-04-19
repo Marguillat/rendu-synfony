00b54b5c5d3e68e04fafdfe42aefcf075f7a5b8c
Margouillat <erwanduchene@gmail.com>
2025-04-19 16:21:35 +0200

    new article début

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -27,6 +30,25 @@ final class ArticleController extends AbstractController
 
     return $this->render("article/unique.html.twig", [
       "article" => $article,
+    ]);
+  }
+
+  #[Route("/cms/add-article", name: "app_new_article")]
+  public function addArticle(EntityManagerInterface $em): Response
+  {
+    // on créé un objet article par défaut
+    $article = new Article();
+
+    //ajouter les champs au formulaire
+    $form = $this->createFormBuilder($article)
+      ->add('title', TextType::class, ['label' => "Titre de l'article"])
+      ->add('content', TextareaType::class, ['label' => "Contenu de l'article"])
+      ->add('save', SubmitType::class, ['label' => "Create article"])
+      ->getForm();
+
+    // ...
+    return $this->render("article/new.html.twig", [
+      "form" => $form
     ]);
   }
 }
