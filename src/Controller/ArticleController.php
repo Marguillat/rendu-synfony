9b1f039adb12f335a2ac450347ccf3c7c0c1c635
Margouillat <erwanduchene@gmail.com>
2025-04-19 19:38:09 +0200

    ajouter article fonctionne

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use function Symfony\Component\Clock\now;
+
 final class ArticleController extends AbstractController
 {
+  /**
+   * Permet d'accéder à la partie sécurité et session dans les pages articles
+   * @var Security
+   */
+  private $security;
+  public function __construct(Security $security)
+  {
+    $this->security = $security;
+  }
+
   #[Route("/articles", name: "app_articles")]
   public function index(EntityManagerInterface $em): Response
   {
@@ -34,8 +48,12 @@ final class ArticleController extends AbstractController
   }
 
   #[Route("/cms/add-article", name: "app_new_article")]
-  public function addArticle(EntityManagerInterface $em): Response
+  public function addArticle(Request $request, EntityManagerInterface $em): Response
   {
+
+    // Récupère le user depuis la session
+    $user = $this->security->getUser();
+
     // on créé un objet article par défaut
     $article = new Article();
 
@@ -43,12 +61,25 @@ final class ArticleController extends AbstractController
     $form = $this->createFormBuilder($article)
       ->add('title', TextType::class, ['label' => "Titre de l'article"])
       ->add('content', TextareaType::class, ['label' => "Contenu de l'article"])
-      ->add('save', SubmitType::class, ['label' => "Create article"])
+      ->add('save', SubmitType::class, ['label' => "Créer le nouvel article"])
       ->getForm();
 
-    // ...
+    // Manipule le formulaire envoyé
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      // $form->getData() holds the submitted values
+      // but, the original `$article` variable has also been updated
+
+      $article->setAuthor($user);
+      $article->setCreatedAt(new \DateTimeImmutable());
+
+      $em->persist($article);
+      // ne pas oublier de tirer la chasse avant de partir
+      $em->flush();
+      return $this->redirectToRoute('app_articles');
+    }
     return $this->render("article/new.html.twig", [
-      "form" => $form
+      "form" => $form,
     ]);
   }
 }
