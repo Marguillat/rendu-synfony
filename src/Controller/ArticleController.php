37c6039d8a95b5f69a4e1ca193b3bcbe44719959
Louka Lemonnier <lk.lemonnier@gmail.com>
2025-04-21 17:18:11 +0200

    feat: update articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,13 +22,46 @@ final class ArticleController extends AbstractController
     ]);
   }
 
-  #[Route("/articles/{id}", name: "app_article")]
+  #[Route("/articles/{id}", name: "app_article", requirements: ["id" => "\d+"])]
   public function articles(EntityManagerInterface $em, int $id): Response
   {
     $article = $em->getRepository(Article::class)->find($id);
 
     return $this->render("article/unique.html.twig", [
       "article" => $article,
+    ]);
+  }
+
+  #[Route("/articles/{id}/edit", name: "app_article_edit")]
+  public function editArticle(EntityManagerInterface $em, int $id, Request $request): Response
+  {
+    $article = $em->getRepository(Article::class)->find($id);
+
+    if (!$article) {
+      throw $this->createNotFoundException('Article not found');
+    }
+
+    $form = $this->createForm(ArticleType::class, $article);
+    $form->handleRequest($request);
+    // Si le formulaire est soumis et est valide, on peut traiter les données et persister les modifications dans la base de données
+    // Si le formulaire n'est pas soumis ou n'est pas valide, on affiche le formulaire avec les données de l'article existant
+
+    if ($form->isSubmitted() && !$form->isValid()) {
+      dump($form->getErrors(true, false)); // Affiche toutes les erreurs du formulaire
+      die();
+    }
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      // Persiste les modifications dans la base de données
+      $em->flush();
+
+      // Redirige vers la page de l'article ou une autre page
+      return $this->redirectToRoute('app_article', ['id' => $article->getId()]);
+    }
+
+    return $this->render("article/edit.html.twig", [
+      "article" => $article,
+      "form" => $form->createView(),
     ]);
   }
 }
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Content',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+            'csrf_protection' => true, // Active la protection CSRF
+            'csrf_field_name' => '_token', // Nom du champ CSRF
+            'csrf_token_id'   => 'article_item', // Identifiant unique pour ce formulaire
+        ]);
+    }
+}
